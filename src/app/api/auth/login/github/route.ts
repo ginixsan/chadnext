a1a087a4f5591eed5699e5616b10d197eb6801ad
Ginés Sanz <ginixsan@users.noreply.github.com>
2024-04-15 19:01:27 +0200

    Initial commit

---
+++ src/app/api/auth/login/github/route.ts
@@ -0,0 +1,18 @@
+import { generateState } from "arctic";
+import { cookies } from "next/headers";
+import { github } from "~/lib/lucia";
+
+export const GET = async () => {
+  const state = generateState();
+  const url = await github.createAuthorizationURL(state);
+
+  cookies().set("github_oauth_state", state, {
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: "lax",
+  });
+
+  return Response.redirect(url);
+};
